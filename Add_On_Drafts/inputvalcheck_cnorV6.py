40f8c95991c70c88bf0563696f5d43bfd6d8cd4b
ns5sonny <90292373+ns5sonny@users.noreply.github.com>
2024-02-22 10:56:49 -0500

    Version 6
    This properly detects strlen and strcpy in same scope for test_2.c but breaks if run on test_1.c

---
+++ Add_On_Drafts/inputvalcheck_cnorV6.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+#
+# Find strlen then see if strcpy exists in same scope
+#
+# Strategy taken from findcasts.py
+
+import cppcheck
+
+@cppcheck.checker
+def compCheck(cfg, data):
+    lengthCheckCopy = 'false'
+    for token in cfg.tokenlist:
+        # Detection to see if we can identify a comparison (e.g. strlen < some number)
+        # These were just used to see if I could find specific items in the tokenlist
+        """ if token.isComparisonOp:
+            cppcheck.reportError(token, 'information', 'found a comparison') 
+        if token.str == 'strlen' and token.next.link.next.isComparisonOp:
+            cppcheck.reportError(token, 'information', 'strlen being compared') """
+        #if token.str == 'strlen':
+        #    cppcheck.reportError(token.astParent, 'information', 'strlen used')
+        if token.isComparisonOp and token.next.isNumber and token.astOperand2.next.link.next.str == 'strlen':
+            cppcheck.reportError(token, 'information', 'forward comparing strlen to a number')
+            checkScope = token.next.next.next.scope
+        # If I have one I can't have the other, we need a way to check regardless of direction
+        """ if token.isComparisonOp: #and token.astOperand1.isNumber and token.next.str == 'strlen':
+            cppcheeck.reportError(token, 'information', 'backward comparing strlen') """
+        if token.isName and token.str == 'strcpy' and token.scope == checkScope:
+            cppcheck.reportError(token, 'information', 'strlen check and strcpy are in same scope')
+            lengthCheckCopy = 'true'
+        if token.isName and token.str == 'strcpy' and lengthCheckCopy == 'false':
+            cppcheck.reportError(token, 'information', 'strcpy does not have proper guards')
+        
+
+#@cppcheck.checker
+def inputval_checker(cfg, data):
+    for token in cfg.tokenlist:
+        if token.isName and token.str == 'scanf':
+            cppcheck.reportError(token, 'information', 'found an input, scanf is not secure')
+        if token.isName and token.str == 'fgets':
+            cppcheck.reportError(token, 'information', 'found an input, fgets requires validation')
+        if token.isName and token.str == 'strcpy':
+            cppcheck.reportError(token, 'information', 'this is a potentially unsafe function! Ensure proper guards')
+        
+        
+@cppcheck.checker # it took an embarrassingly long time to figure out I need this line before every method or they won't run
+# Method to determine if proper guards are being used for strcpy
+def buffOverflowCheck(cfg, data):
+    myLoc = '' # I'd like to be able to identify where the If statement is outside its loop
+    for scope in cfg.scopes:
+        if scope.type == 'If':
+            myLoc = scope.bodyStart
+            #cppcheck.reportError(myLoc, 'information', 'found an If statement with scoping')
+            # ideally I'd like to be checking to see if strcpy is used inside this statement
+        #if scope.type == 'Else': # just to see if this works too
+        #    cppcheck.reportError(scope.bodyStart, 'information', 'caught by Else')
+
+    """ for token in cfg.tokenlist:
+        while token.str != myLoc:
+            if token.isName and token.str == 'strcpy':
+                cppcheck.reportError(token, 'information', 'found strcpy inside if statement') """
+
