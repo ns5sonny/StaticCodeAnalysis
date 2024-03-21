2df6b687701315fb30b4b87d9cd64b55c3e3297c
ns5sonny <90292373+ns5sonny@users.noreply.github.com>
2024-03-21 11:43:59 -0400

    V 10
    Covers all test files, still missing test_4_1.c

--- Add_On_Drafts/inputvalcheck_cnorV10.py
+++ Add_On_Drafts/inputvalcheck_cnorV10.py
@@ -8,8 +8,10 @@ import cppcheck
 
 @cppcheck.checker
 def strcpyGuarded(cfg, data):
+    # Initialization of checked variables
     lengthCheckCpy = 'false'
     properChecks = 'false'
+    sameLine = 'false'
 
     checkScope = "0"
     compScope = "1"
@@ -19,42 +21,50 @@ def strcpyGuarded(cfg, data):
     ifStateScope = "4"
     elseBodyScope = "5"
 
+    strlenLnNbr = "6"
+    compOpLnNbr = "7"
+
     #numScope = "0"
     for scope in cfg.scopes:
         if scope.type == "If":
             ifStateScope = scope.nestedIn
             ifBodyScope = scope
             #cppcheck.reportError(scope.bodyStart, 'information', 'found a scope')
-        if scope.type == 'Else':
+        if scope.type == "Else":
             elseBodyScope = scope
 
 
     for token in cfg.tokenlist:
-        # Detection to see if we can identify a comparison (e.g. strlen < some number)
-
         if token.isComparisonOp and token.astParent.str == "(":
             compScope = token.scope # scope inside if statement
             checkScope = token.astParent.link.next.scope # the parent is the first (, then link to 
             # the ), then .next with be the { of the body, which should be the same as ifBodyScope
-        
+            compOpLnNbr = token.linenr
+
         if token.str == 'strlen':
             lengthScope = token.scope
+            strlenLnNbr = token.linenr
+            cppcheck.reportError(token, 'information', 'testing: ' + str(strlenLnNbr))
+
         if compScope == lengthScope:
             properChecks = 'true'
             # Shows we have a comparison to strlen in the same scope
+
+        if str(strlenLnNbr) == str(compOpLnNbr):
+            sameLine = 'true'
+            # Ensuring the strlen is used in a comparison "if statement"
 
         # testing scope loop successes
         #if token.str == 'strcpy' and (token.scope == ifBodyScope or token.scope == elseBodyScope):
         #    cppcheck.reportError(token, 'information', 'found the scope for strcpy yay!')
         #if token.isComparisonOp and token.scope == ifStateScope:
         #    cppcheck.reportError(token, 'information', 'found right scope for comp')
-        #if token.str == 'strlen' and token.linenr
-        # reminder to use linenr to ensure comp and strlen in same statement
 
 
 
-        if token.isName and token.str == 'strcpy' and (token.scope == checkScope or token.scope == elseBodyScope):
-            cppcheck.reportError(token, 'information', 'strcpy is in scope "if( < ){ or else{ ')
+
+        if (token.isName and token.str == 'strcpy') and ((properChecks == 'true' and sameLine == 'true') or token.scope == elseBodyScope):
+            cppcheck.reportError(token, 'information', 'strcpy is in scope "if( < ){" or "else{" : properly guarded ')
             # extra "error" to show our checks are happening properly, i.e. success state
             lengthCheckCpy = 'true'
 
